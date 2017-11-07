0d5362696f637d8853789ef1d74248334fdcfbc9
unknown <zijunzhao8@gmail.com>
2017-11-07 15:06:07 +0000

    init

---
+++ test/io/SimpleUCPTest.java
@@ -0,0 +1,26 @@
+package io;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class SimpleUCPTest {
+	String testStr = "c  simple.cnf\r\n" + "c\r\n" + "p cnf 4 4\r\n" + "1 2 0\r\n" + "-1 3 0\r\n" + "-3 4 0\r\n"
+			+ "1 0";
+	
+	@Test
+	public void test() {
+		Formula bf = new Formula();
+		CNFParser cp = new CNFParser(bf);
+		InputStream is = new ByteArrayInputStream(testStr.getBytes());
+		cp.parse(is);
+		
+		SimpleUCP s = new SimpleUCP(bf);
+		assertTrue(s.run() == 1);
+		
+	}
+
+}
