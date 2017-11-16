c3c6042473f7ecffb85b85b166c7e46e495af52c
Unknown <zijunzhao8@gmail.com>
2017-11-16 02:43:51 +0000

    Clause class

---
+++ src/Clause.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Clause {
+             public Clause() {
+                     this.literals = new ArrayList<Literal>(); 
+}
+          
+             
+
+
+
+
+
+}
