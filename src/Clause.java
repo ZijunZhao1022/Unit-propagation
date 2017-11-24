02f83048f21b281c0849c8ce60e57e7383dc368b
Unknown <zijunzhao8@gmail.com>
2017-11-24 05:17:59 +0000

    Clause :write some code

--- src/Clause.java
+++ src/Clause.java
@@ -3,9 +3,19 @@ import java.util.ArrayList;
 public class Clause {
              public Clause() {
                      this.literals = new ArrayList<Literal>(); 
-}
-          
+             }
              
+             public Clause(ArrayList<Literal> literals) {
+                     this.literals = literals;
+
+             }
+             public void addLiteral(int symbol, boolean sign) {
+                     literals.add(new Literal (symbol, sign))
+             } 
+             public void addLiteral(int symbol) {
+                     if (symbol < 0){}
+                     else
+}
 
 
 
