fa1fe9ab2958f7b65688fee2264c6b18ebb0d92b
unknown <zijunzhao8@gmail.com>
2017-11-26 14:46:14 +0000

    Clause code

--- src/Clause.java
+++ src/Clause.java
@@ -24,7 +24,21 @@ public class Clause {
 		return literals.contains(l);
 	     }
 
-
+             public void removeUnit(Literal unit) {
+		int size = literals.size();
+		int index = -1;
+		for(int i=0;i<size;++i) {
+			if(literals.get(i).equals(unit))
+				index = i;
+		}
+		if(index > -1)
+		{
+			if(unit.sign != literals.get(index).sign)
+				literals.remove(index);
+			else
+				literals.clear();
+		}
+	}
 
 
 
