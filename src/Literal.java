03ff7f1d57f0f9c7c52e0481761aeceb7f377fca
unknown <zijunzhao8@gmail.com>
2017-12-07 21:42:26 +0000

    creating a Literal

--- src/Literal.java
+++ src/Literal.java
@@ -1,9 +1,30 @@
 public class Literal {
 
         public Literal(int symbol, boolean sign){
-
+		System.out.println("add new literal s = " + symbol + " sign = " + sign);
+		this.symbol = symbol;
+		this.sign = sign;
 
 } 
+public String toString() {
+		String s ="";
+		if(sign == false) {
+			s+="-"+ symbol;
+		}
+		else {
+			s+=symbol;
+		}
+		return s;
+	}
+	
+	public boolean equals(Literal l) {
+		return l.symbol == this.symbol;
+	}
+	
+	/** sign of the literal: true = unnegated, false = negated */
+	public boolean sign;
 
+	/** index of proposition symbol */
+	public int symbol
 
 }
